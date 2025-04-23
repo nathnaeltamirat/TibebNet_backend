23aa0db4b3f0db7724332a0eef9a86775f485ef8
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-04-23 10:59:02 +0300

    error handing middlewares

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { errorHandler, errorConverter } = require("./middlewares/error");
+const { status } = require("http-status");
+const CustomError = require("./utils/customError");
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+// Import routes
+
+//error handler middleware
+app.use((req, res, next) => {
+  new CustomError(status.NOT_FOUND, "Not Found");
+});
+app.use(errorConverter);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ src/utils/customError.js
@@ -0,0 +1,14 @@
+class CustomError extends Error {
+  constructor(statusCode, message, isOperational = true, stack = "") {
+    super(message);
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+  }
+}
+
+module.exports = CustomError;
