d05baf741e07cfb83318f438679ff7860599a0d8
MeryDemissie <149376575+MeryDemissie@users.noreply.github.com>
2025-05-02 17:41:53 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const communityRoutes = require("./routes/community");
+const app = require("./app");
 
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.use(morgan("dev"));
-
-
-app.get("/", (req, res) => {
-  res.send("Hello, TibebNet Community API is running!");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-// Error handling
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  res.status(statusCode).json({ message });
-});
-
-module.exports = app;
--- src/routes/community.routes.js
+++ src/routes/community.routes.js
@@ -4,12 +4,12 @@ const communityController = require("../controllers/community.controller");
 const { protect } = require("../middlewares/auth");  
 
 
-router.post("/create", protect, communityController.createCommunity);
+router.post("/create", /*protect*/communityController.createCommunity);
 
 
-router.post("/add-member", protect, communityController.addMember);
+router.post("/add-member", /*protect*/communityController.addMember);
 
 
-router.post("/remove-member", protect, communityController.removeMember);
+router.post("/remove-member", /*protect*/ communityController.removeMember);
 
 module.exports = router;
